65c0d10510b22d6861257d69f398754eee79c3b6
Shashi <shashi.vish@yahoo.in>
2018-11-28 00:42:50 +0800

    Configure producer

--- KinesisDataProducer/KinesisProducer/src/main/java/com/grab/kinesis/KinesisProducer/GrabKinensProducer.java
+++ KinesisDataProducer/KinesisProducer/src/main/java/com/grab/kinesis/KinesisProducer/GrabKinensProducer.java
@@ -9,41 +9,48 @@ import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
 
 public class GrabKinensProducer {
 
-	  /**
-     * Timestamp we'll attach to every record
-     */
-    private static final String TIMESTAMP = Long.toString(System.currentTimeMillis());
-    
-    /**
-     * Change these to try larger or smaller records.
-     */
-    private static final int DATA_SIZE = 128;
-    
-    /**
-     * Put records for this number of seconds before exiting.
-     */
-    private static final int SECONDS_TO_RUN = 5;
-   
-  
-    private static final int RECORDS_PER_SECOND = 2000;
-    
-    /**
-     * Change this to your stream name.
-     */
-    public static final String STREAM_NAME = "test";
-    
-    /**
-     * Change this to the region you are using.
-     */
-    public static final String REGION = "eu-central-1";
-    
+	/**
+	 * Timestamp we'll attach to every record
+	 */
+	private static final String TIMESTAMP = Long.toString(System.currentTimeMillis());
+
+	/**
+	 * Change these to try larger or smaller records.
+	 */
+	private static final int DATA_SIZE = 128;
+
+	/**
+	 * Put records for this number of seconds before exiting.
+	 */
+	private static final int SECONDS_TO_RUN = 5;
+
+
+	private static final int RECORDS_PER_SECOND = 2000;
+
+	/**
+	 * Change this to your stream name.
+	 */
+	public static final String STREAM_NAME = "test";
+
+	/**
+	 * Change this to the region you are using.
+	 */
+	public static final String REGION = "eu-central-1";
+
 	public static void main(String args[]) throws UnsupportedEncodingException
 
 	{
 		try
 		{
+			System.setProperty("aws.secretKey", "8RfmF4d7BCVy8r4qPWbU6rQJ0db2kL4cBiQ");
+			System.setProperty("aws.accessKeyId", "AKIAJW6QNQBLPROZZDGQ");
+
 			System.out.println("Creatiing Connection");
-			KinesisProducer kinesis = new KinesisProducer();  
+
+			GrabKinensProducer grabKinesisProvide =new GrabKinensProducer();
+
+			KinesisProducer kinesis =  grabKinesisProvide.getKinesisProducer();
+
 			// Put some records 
 			for (int i = 0; i < 100; ++i) {
 				ByteBuffer data = ByteBuffer.wrap("myData".getBytes("UTF-8"));
@@ -56,36 +63,32 @@ public class GrabKinensProducer {
 		{
 			e.printStackTrace();	
 		}
-		
+
 
 	}
-	
-	
 
 
- 
-    public static KinesisProducer getKinesisProducer() {
-        
-        KinesisProducerConfiguration config = new KinesisProducerConfiguration();
-        
-       
-        config.setRegion(REGION);
-        
-       
-        config.setCredentialsProvider(new DefaultAWSCredentialsProviderChain());
-        
-        
-        config.setMaxConnections(1);
-        
-       
-        config.setRequestTimeout(60000);
-        
-       
-        config.setRecordMaxBufferedTime(15000);
+	public static KinesisProducer getKinesisProducer() {
 
-       
-        KinesisProducer producer = new KinesisProducer(config);
-        
-        return producer;
-    }
+		KinesisProducerConfiguration config = new KinesisProducerConfiguration();
+
+		config.setRegion(REGION);
+
+
+		config.setCredentialsProvider(new DefaultAWSCredentialsProviderChain());
+
+
+		config.setMaxConnections(1);
+
+
+		config.setRequestTimeout(60000);
+
+
+		config.setRecordMaxBufferedTime(15000);
+
+
+		KinesisProducer producer = new KinesisProducer(config);
+
+		return producer;
+	}
 }
